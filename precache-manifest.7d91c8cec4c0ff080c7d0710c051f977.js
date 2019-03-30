97790670f81a6dafad14133c06eb390cfe8068e2
sudipto ranjan <sudiptoranjan123@gmail.com>
2019-03-30 21:50:26 +0600

    Updates

---
+++ precache-manifest.7d91c8cec4c0ff080c7d0710c051f977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27034129dafbd80d2a68",
+    "url": "/video_search/static/js/runtime~main.b23aa1c6.js"
+  },
+  {
+    "revision": "7b3e89221804a3ef2f85",
+    "url": "/video_search/static/js/main.28df4e54.chunk.js"
+  },
+  {
+    "revision": "b9572d93d49208d17f63",
+    "url": "/video_search/static/js/2.1cfc7d0f.chunk.js"
+  },
+  {
+    "revision": "7b3e89221804a3ef2f85",
+    "url": "/video_search/static/css/main.56ab007f.chunk.css"
+  },
+  {
+    "revision": "b8fa0bdc1290b00daeba6b804d5f64ac",
+    "url": "/video_search/index.html"
+  }
+];
